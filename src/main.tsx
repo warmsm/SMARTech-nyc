1c01179fa71991a5ec8ac98d9ebc51b86e3b3231
warmsm <78923727+warmsm@users.noreply.github.com>
2026-05-03 00:35:09 +0800

    App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,7 @@
-import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
-// Import your providers so the app doesn't crash on 'useAuth' or 'usePosts'
-import { AuthProvider } from "./contexts/AuthContext";
-import { PostsProvider } from "./contexts/PostsContext";
 
-// Import the page you want to show as the landing page
-import { PubMatsPage } from "./app/pages/PubMatsPage"; 
+  import { createRoot } from "react-dom/client";
+  import App from "./app/App.tsx";
+  import "./styles/index.css";
 
-import "./styles/index.css";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <AuthProvider>
-      <PostsProvider>
-        {/* Rendering PubMatsPage directly since App.tsx is missing */}
-        <PubMatsPage />
-      </PostsProvider>
-    </AuthProvider>
-  </StrictMode>
-);
+  createRoot(document.getElementById("root")!).render(<App />);
+  
